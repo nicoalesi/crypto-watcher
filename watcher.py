a087e35c90ed38a34bad7322c6392193d2e827f3
Nico <nicoalesi.cs@gmail.com>
2025-02-10 21:41:23 +0100

    feat: add layout
    Add layout. Every crypto has its own row and every row is composed by a grid.

--- watcher.py
+++ watcher.py
@@ -7,17 +7,18 @@ from tkinter import ttk
 window = tk.Tk()
 windll.shcore.SetProcessDpiAwareness(1)
 window.title("Crypto watcher")
-window.geometry("1400x800")
+window.geometry("1200x740")
+window.resizable(False, False)
 
 # Set up main font
-font = font.Font(family = "Cascadia Code", size = 20)
+title_font = font.Font(family = "Cascadia Code", size = 20)
 
 # Set up main title
-title_label = ttk.Label(master = window, text = "Crypto watcher", font = font)
+title_label = ttk.Label(master = window, text = "Crypto watcher", font = title_font)
 title_label.pack(fill = tk.Y, pady = 10)
 
 # Set up canvas
-canvas = tk.Canvas(master = window, scrollregion = (0, 0, 1400, 1600))
+canvas = tk.Canvas(master = window, scrollregion = (0, 0, 1400, 1700))
 
 # Set up scrollbar
 scrollbar = tk.Scrollbar(master = window, orient = "vertical", command = canvas.yview)
@@ -27,6 +28,58 @@ scrollbar.place(relx = 1, rely = 0, relheight = 1, anchor = "ne")
 # Set up mousewheel scrolling
 canvas.bind("<MouseWheel>", lambda event: canvas.yview_scroll(-int(event.delta / 120), "units"))
 
+# Set up row fonts
+row_name_font = font.Font(family = "Cascadia Code", size = 36)
+row_symbol_font = font.Font(family = "Cascadia Code", size = 24)
+row_price_font = font.Font(family = "Cascadia Code", size = 24)
+row_price_change_font = font.Font(family = "Cascadia Code", size = 18)
+row_button_font = font.Font(family = "Cascadia Code", size = 15)
+
+# Create crypto's row
+def create_row(title, symbol, data):
+    # Set up row's frame
+    row_frame = tk.Frame(master = canvas)
+
+    # Set up crypto's name label
+    name_label = ttk.Label(master = row_frame, text = title, font = row_name_font, width = 10)
+    name_label.grid(row = 0, column = 0, rowspan = 2, sticky = tk.SW, padx = 10)
+
+    # Set up crypto's symbol label
+    symbol_label = ttk.Label(master = row_frame, text = symbol, font = row_symbol_font, width = 10)
+    symbol_label.grid(row = 2, column = 0, rowspan = 2, sticky = tk.NW, padx = 10)
+
+    # Set up canvas for graph
+    row_canvas = tk.Canvas(master = row_frame, height = 120, width = 500)
+    row_canvas.configure(bg="red")
+    row_canvas.grid(row = 0, column = 1, rowspan = 4, padx = 10)
+
+    # Set up crypto's price label
+    price_label = ttk.Label(master = row_frame, text = "123,43.23", font = row_price_font, width = 12, anchor = tk.SE)
+    price_label.grid(row = 0, column = 2, columnspan = 3, sticky = tk.SE, padx = 10)
+
+    # Set up crypto's price change label
+    price_change_label = ttk.Label(master = row_frame, text = "2.3%", font = row_price_change_font, width = 10, anchor = tk.E)
+    price_change_label.grid(row = 1, column = 2, columnspan = 3, sticky = tk.E, padx = 10)
+
+    # Set up time buttons
+    day_button = tk.Button(master = row_frame, text = "1D", height = 2, width = 4, bd = 0, font = row_button_font)
+    month_button = tk.Button(master = row_frame, text = "1M", height = 2, width = 4, bd = 0, font = row_button_font)
+    year_button = tk.Button(master = row_frame, text = "1Y", height = 2, width = 4, bd = 0, font = row_button_font)
+
+    day_button.grid(row = 2, column = 2, rowspan = 2, sticky = tk.E)
+    month_button.grid(row = 2, column = 3, rowspan = 2, sticky = tk.E)
+    year_button.grid(row = 2, column = 4, rowspan = 2, sticky = tk.E)
+
+    # Return crypto's frame
+    return row_frame
+
+items = ["Bitcoin", "Ethereum", "Ripple", "Solana", "Dogecoin", "Shiba Inu", "Litecoin"]
+symbols = ["BTC", "ETH", "XRP", "SOL", "DOGE", "SHIB", "LTC"]
+
+for i, text in enumerate(items):
+    canvas.create_window(580, 100 + i * 250, window = create_row(text, symbols[i], {}))
+
+# Pack canvas
 canvas.pack(expand = True, fill = "both", anchor = "ne")
 
 # Run
