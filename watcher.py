3c9a5448cb365430b2332c64a331d731a1f1746e
Nico <nicoalesi.cs@gmail.com>
2025-02-10 19:31:48 +0100

    feat: add title label
    Add title label at the top of the window.

--- watcher.py
+++ watcher.py
@@ -1,10 +1,20 @@
+from ctypes import windll
 import tkinter as tk
+from tkinter import font
 from tkinter import ttk
 
-# Window
+# Set up window
 window = tk.Tk()
+windll.shcore.SetProcessDpiAwareness(1)
 window.title("Crypto watcher")
-window.geometry("600x400")
+window.geometry("1400x800")
+
+# Set up main font
+font = font.Font(family = "Cascadia Code", size = 20)
+
+# Set up main title
+title_label = ttk.Label(master = window, text = "Crypto watcher", font = font)
+title_label.pack()
 
 # Run
 window.mainloop()
