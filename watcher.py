80157c43fc2264007c6eb8421d9fdd6d1d041206
Nico <nicoalesi.cs@gmail.com>
2025-02-10 19:44:22 +0100

    feat: add ability to scroll
    Add ability to scroll using both the scrollbar and the mouse wheel.

--- watcher.py
+++ watcher.py
@@ -14,7 +14,20 @@ font = font.Font(family = "Cascadia Code", size = 20)
 
 # Set up main title
 title_label = ttk.Label(master = window, text = "Crypto watcher", font = font)
-title_label.pack()
+title_label.pack(fill = tk.Y, pady = 10)
+
+# Set up canvas
+canvas = tk.Canvas(master = window, scrollregion = (0, 0, 1400, 1600))
+
+# Set up scrollbar
+scrollbar = tk.Scrollbar(master = window, orient = "vertical", command = canvas.yview)
+canvas.configure(yscrollcommand = scrollbar.set)
+scrollbar.place(relx = 1, rely = 0, relheight = 1, anchor = "ne")
+
+# Set up mousewheel scrolling
+canvas.bind("<MouseWheel>", lambda event: canvas.yview_scroll(-int(event.delta / 120), "units"))
+
+canvas.pack(expand = True, fill = "both", anchor = "ne")
 
 # Run
 window.mainloop()
